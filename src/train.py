612303e8e86df7be10909446eb3a64e07be49700
WooSeok Kim <3suksw@gmail.com>
2024-04-03 13:59:19 +0900

    update train.py

--- src/train.py
+++ src/train.py
@@ -8,38 +8,25 @@ from pathlib import Path
 from pytorch_lightning import Trainer
 
 
-def test():
-    data = BatteryDataModule(data_dir="data/nsc7hnsg4s-2", type="single", batch_size=64)
-    # test()
-    # data = BatteryDataModule(data_dir="test/", type="single", batch_size=64)
-    dataset = data.training_dataset
-
-    for discharge in dataset:
-        tt = discharge["tt"]
-        yy = discharge["yy"]
-        time = discharge["time"]
-        voltage = discharge["voltage"]
-        plt.plot(time, voltage, color="r", label="input")
-        plt.plot(tt, yy, color="b", label="context")
-        if len(tt) == 0 or len(time) == 0:
-            print("error")
-
-    plt.show()
-    print(dataset.__len__())
-
-
 def main():
-    # data_dir = "data/nsc7hnsg4s-2"
-    data_dir = "test/"
+    data_dir = "data/"
 
     model = Dynaformer()
-    data = BatteryDataModule(data_dir=data_dir, type="single", num_w=4)
+
+    data = BatteryDataModule(
+        discharge_type="single",
+        data_dir=data_dir,
+        dataset_name="HUST",
+        batch_size=64,
+        num_w=4,
+    )
+
     trainer = Trainer(
         accelerator="mps",
         num_sanity_val_steps=0,
-        # log_every_n_steps=10,
+        check_val_every_n_epoch=5,
         log_every_n_steps=1,
-        max_epochs=50,
+        max_epochs=10001,
     )
 
     trainer.fit(model, data)
