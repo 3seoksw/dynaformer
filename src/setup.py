6308ff3f79052a32b18f1d8627861b31037fdcb4
WooSeok Kim <3suksw@gmail.com>
2024-04-05 02:25:37 +0900

    update

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    version="1.0.0",
+    description="",
+    name="dynaformer",
+    packages=setuptools.find_packages("."),
+    package_dir={"": "."},
+    install_requires=[],
+)
